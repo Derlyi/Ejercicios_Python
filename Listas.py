306b4f387df7904cb3452946a4d36f9cbc5b834a
Derly Valeria Huelgos Rojas <134748644+Derlyi@users.noreply.github.com>
2024-07-31 22:05:29 -0500

    listas

---
+++ Listas.py
@@ -0,0 +1,30 @@
+'''empty_list = []
+
+languages = ['Python', 'Ruby', 'Javascript']
+
+fibonacci = [0, 1, 1, 2, 3, 5, 8, 13]
+
+data = ['Tenerife', {'cielo': 'limpio', 'temp': 24}, 3718, (28.2933947, -16.5226597)]'''
+
+
+#carrito de compra
+
+'''shopping = ['Agua', 'Huevos', 'Aceite']
+
+shopping[0]
+
+shopping[1]
+
+shopping[2]
+
+shopping[-1]  # acceso con índice negativo'''
+
+
+'''even_numbers = []
+
+for i in range(20):
+    if i % 2 == 0:
+        even_numbers.append(i)
+
+print(even_numbers)'''
+
